1aa0b54c850a1291d9c47c5997a40831bd0820d3
Jacob Milling <JacobDexter-Milling@Jacobs-MacBook-Air.local>
2016-05-02 15:46:15 -0700

    completed layouts for UI

--- Omni-Vision/app/src/main/java/com/example/omni_vision/MainActivity.java
+++ Omni-Vision/app/src/main/java/com/example/omni_vision/MainActivity.java
@@ -3,20 +3,35 @@ package com.example.omni_vision;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
-    private SurfaceView preview=null;
-    private SurfaceHolder previewHolder=null;
-    private Camera camera=null;
-    private boolean inPreview=false;
-    private boolean cameraConfigured=false;
+    /**
+     * Camera related declarations.
+     */
+    private SurfaceView preview = null;
+    private SurfaceHolder previewHolder = null;
+    private Camera camera = null;
+    private boolean inPreview = false;
+    private boolean cameraConfigured = false;
+
+    /**
+     * Main menu button declarations.
+     */
+    Button leftMainButton;
+    Button wikiPediaButton;
+    Button gmailButton;
+    Button rightMainButton;
+    Button youTubeButton;
+    Button mapsButton;
 
 
 
@@ -25,18 +40,35 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        preview=(SurfaceView)findViewById(R.id.surfaceview);
-        previewHolder=preview.getHolder();
+        // Setting up the camera.
+        preview = (SurfaceView) findViewById(R.id.surfaceview);
+        previewHolder = preview.getHolder();
         previewHolder.addCallback(surfaceCallback);
         previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         camera = Camera.open();
 
+        // Getting the main menu button on top (in front of) camera preview
+        leftMainButton = (Button) findViewById(R.id.leftMainButton);
+        leftMainButton.bringToFront();
+        preview.invalidate();
+
+        //Instantiating and setting up main menu buttons.
+        wikiPediaButton = (Button) findViewById(R.id.wikiPediaButton);
+        wikiPediaButton.setVisibility(View.GONE);
+        gmailButton = (Button) findViewById(R.id.gmailButton);
+        gmailButton.setVisibility(View.GONE);
+        rightMainButton = (Button) findViewById(R.id.rightMainButton);
+        youTubeButton = (Button) findViewById(R.id.youTubeButton);
+        youTubeButton.setVisibility(View.GONE);
+        mapsButton = (Button) findViewById(R.id.mapsButton);
+        mapsButton.setVisibility(View.GONE);
+
     }
 
     /**
-     *----------------End OnCreate-----------------------------------------------------------------
-     *
-     *----------------Start Methods----------------------------------------------------------------
+     * ----------------End OnCreate-----------------------------------------------------------------
+     * <p/>
+     * ----------------Start Methods----------------------------------------------------------------
      */
 
 
@@ -56,8 +88,8 @@ public class MainActivity extends AppCompatActivity{
         }
 
         camera.release();
-        camera=null;
-        inPreview=false;
+        camera = null;
+        inPreview = false;
 
         super.onPause();
     }
@@ -72,6 +104,7 @@ public class MainActivity extends AppCompatActivity{
     /**
      * Hides the Status bar and soft keys.
      * Causes non-fatal errors.
+     *
      * @param hasFocus
      */
 
@@ -90,31 +123,29 @@ public class MainActivity extends AppCompatActivity{
     }
 
     private Camera.Size getBestPreviewSize(int width, int height, Camera.Parameters parameters) {
-        Camera.Size result=null;
+        Camera.Size result = null;
         for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
-            if (size.width<=width && size.height<=height) {
-                if (result==null) {
-                    result=size;
-                }
-                else {
-                    int resultArea=result.width*result.height;
-                    int newArea=size.width*size.height;
+            if (size.width <= width && size.height <= height) {
+                if (result == null) {
+                    result = size;
+                } else {
+                    int resultArea = result.width * result.height;
+                    int newArea = size.width * size.height;
 
-                    if (newArea>resultArea) {
-                        result=size;
+                    if (newArea > resultArea) {
+                        result = size;
                     }
                 }
             }
         }
-        return(result);
+        return (result);
     }
 
     private void initPreview(int width, int height) {
-        if (camera!=null && previewHolder.getSurface()!=null) {
+        if (camera != null && previewHolder.getSurface() != null) {
             try {
                 camera.setPreviewDisplay(previewHolder);
-            }
-            catch (Throwable t) {
+            } catch (Throwable t) {
                 Log.e("MainActivity",
                         "Exception in setPreviewDisplay()", t);
                 Toast
@@ -122,35 +153,37 @@ public class MainActivity extends AppCompatActivity{
                         .show();
             }
             if (!cameraConfigured) {
-                Camera.Parameters parameters=camera.getParameters();
-                Camera.Size size=getBestPreviewSize(width, height,
+                Camera.Parameters parameters = camera.getParameters();
+                Camera.Size size = getBestPreviewSize(width, height,
                         parameters);
-                if (size!=null) {
+                if (size != null) {
                     parameters.setPreviewSize(size.width, size.height);
                     camera.setParameters(parameters);
-                    cameraConfigured=true;
+                    cameraConfigured = true;
                 }
             }
         }
     }
 
     private void startPreview() {
-        if (cameraConfigured && camera!=null) {
+        if (cameraConfigured && camera != null) {
             camera.startPreview();
-            inPreview=true;
+            inPreview = true;
         }
     }
 
-    SurfaceHolder.Callback surfaceCallback=new SurfaceHolder.Callback() {
+    SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
         public void surfaceCreated(SurfaceHolder holder) {
             // no-op -- wait until surfaceChanged()
         }
+
         public void surfaceChanged(SurfaceHolder holder,
                                    int format, int width,
                                    int height) {
             initPreview(width, height);
             startPreview();
         }
+
         public void surfaceDestroyed(SurfaceHolder holder) {
             // no-op
         }
