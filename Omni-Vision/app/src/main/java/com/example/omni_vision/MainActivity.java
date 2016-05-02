d28bf7ed15a9ec6106aede1a2d0be7f01d4d8c85
Jacob Milling <JacobDexter-Milling@Jacobs-MacBook-Air.local>
2016-05-02 12:32:21 -0700

    added requirement for device to have camera

--- Omni-Vision/app/src/main/java/com/example/omni_vision/MainActivity.java
+++ Omni-Vision/app/src/main/java/com/example/omni_vision/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
 import android.widget.Toast;
 
 
@@ -65,6 +66,26 @@ public class MainActivity extends AppCompatActivity{
         if (camera != null) {
             camera.release();
             camera = null;
+        }
+    }
+
+    /**
+     * Hides the Status bar and soft keys.
+     * Causes non-fatal errors.
+     * @param hasFocus
+     */
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
     }
 
